3614c4b0e3b1e879595704599a1842c022f4962a
Iiro Kajaniemi <kajaniemi.iiro@gmail.com>
2023-02-15 21:04:13 +0200

    comment index.php

--- index.php
+++ index.php
@@ -1,43 +1,53 @@
 <?php
+// Error reporting and display
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
+// Headers for cross-origin
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
 header("Access-Control-Allow-Methods: *");
 
+// Include database connection file
 include 'DbConnect.php';
+
+// Create it as object and connect to the database
 $objDb = new DbConnect;
 $conn = $objDb->connect();
 
+// Get the HTTP method used in the request 
 $method = $_SERVER['REQUEST_METHOD'];
+// Based on the HTTP method, perform CRUD operation
 switch($method) {
     case "GET":
-        $sql = "SELECT * FROM users";
+        // Retrieve events based on ID, on specified URI
+        $sql = "SELECT * FROM events";
         $path = explode('/', $_SERVER['REQUEST_URI']);
         if(isset($path[3]) && is_numeric($path[3])) {
             $sql .= " WHERE id = :id";
             $stmt = $conn->prepare($sql);
             $stmt->bindParam(':id', $path[3]);
             $stmt->execute();
-            $users = $stmt->fetch(PDO::FETCH_ASSOC);
+            $events = $stmt->fetch(PDO::FETCH_ASSOC);
         } else {
             $stmt = $conn->prepare($sql);
             $stmt->execute();
-            $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            $events = $stmt->fetchAll(PDO::FETCH_ASSOC);
         }
-
-        echo json_encode($users);
+        // Encode the retrieved events as JSON and output 
+        echo json_encode($events);
         break;
     case "POST":
-        $user = json_decode( file_get_contents('php://input') );
-        $sql = "INSERT INTO users(id, name, email, mobile, created_at) VALUES(null, :name, :email, :mobile, :created_at)";
+        // create a new event record with data from the request body
+        $event = json_decode( file_get_contents('php://input') );
+        $sql = "INSERT INTO events(id, name, email, mobile, created_at) VALUES(null, :name, :email, :mobile, :created_at)";
         $stmt = $conn->prepare($sql);
         $created_at = date('Y-m-d');
-        $stmt->bindParam(':name', $user->name);
-        $stmt->bindParam(':email', $user->email);
-        $stmt->bindParam(':mobile', $user->mobile);
+        $stmt->bindParam(':name', $event->name);
+        $stmt->bindParam(':email', $event->email);
+        $stmt->bindParam(':mobile', $event->mobile);
         $stmt->bindParam(':created_at', $created_at);
 
+        // check if the event record was successfully created and output response accordingly
         if($stmt->execute()) {
             $response = ['status' => 1, 'message' => 'Record created successfully.'];
         } else {
@@ -47,16 +57,17 @@ switch($method) {
         break;
 
     case "PUT":
-        $user = json_decode( file_get_contents('php://input') );
-        $sql = "UPDATE users SET name= :name, email =:email, mobile =:mobile, updated_at =:updated_at WHERE id = :id";
+        // update an existing event record with data from the request body
+        $event = json_decode( file_get_contents('php://input') );
+        $sql = "UPDATE events SET name= :name, email =:email, mobile =:mobile, updated_at =:updated_at WHERE id = :id";
         $stmt = $conn->prepare($sql);
         $updated_at = date('Y-m-d');
-        $stmt->bindParam(':id', $user->id);
-        $stmt->bindParam(':name', $user->name);
-        $stmt->bindParam(':email', $user->email);
-        $stmt->bindParam(':mobile', $user->mobile);
+        $stmt->bindParam(':id', $event->id);
+        $stmt->bindParam(':name', $event->name);
+        $stmt->bindParam(':email', $event->email);
+        $stmt->bindParam(':mobile', $event->mobile);
         $stmt->bindParam(':updated_at', $updated_at);
-
+    
         if($stmt->execute()) {
             $response = ['status' => 1, 'message' => 'Record updated successfully.'];
         } else {
@@ -66,7 +77,7 @@ switch($method) {
         break;
 
     case "DELETE":
-        $sql = "DELETE FROM users WHERE id = :id";
+        $sql = "DELETE FROM events WHERE id = :id";
         $path = explode('/', $_SERVER['REQUEST_URI']);
 
         $stmt = $conn->prepare($sql);
