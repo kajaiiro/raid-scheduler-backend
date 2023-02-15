31b21158039ace26cf4dd81ffa786657b6999168
Iiro Kajaniemi <kajaniemi.iiro@gmail.com>
2023-02-15 21:04:55 +0200

    comment dbconnect.php

--- DbConnect.php
+++ DbConnect.php
@@ -1,19 +1,20 @@
 <?php
-	/**
-	* Database Connection
-	*/
 	class DbConnect {
-		private $server = 'localhost';
-		private $dbname = 'react_crud';
-		private $user = 'root';
+		// Properties for the connection 
+		private $server = '';
+		private $dbname = '';
+		private $user = '';
 		private $pass = '';
-
+		// PDO Object as database connection
 		public function connect() {
 			try {
+				// New PDO Object with properties
 				$conn = new PDO('mysql:host=' .$this->server .';dbname=' . $this->dbname, $this->user, $this->pass);
 				$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+				// Return PDO Object 
 				return $conn;
 			} catch (\Exception $e) {
+				// If error then, catch it, and display
 				echo "Database Error: " . $e->getMessage();
 			}
 		}
